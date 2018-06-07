f7bde7956fbb7e23c42794e90b2dc57b88d8c9eb
ani <anmol.sin@gmail.com>
2018-06-07 15:36:51 +0530

    All are updated

--- prog.dart
+++ prog.dart
@@ -1,3 +1,16 @@
 main void() {
 	print('Hello');
+	itClass itTest = new itClass();
+	print(itClass.b);
+
+
+}
+
+class itClass{
+	int a = 1;
+	var b = "This is a variable";
+
+	met() {
+		return a.toString() + b;
+	}
 }
